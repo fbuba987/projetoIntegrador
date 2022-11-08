b3f933522eb14b6c7951ccd2e5df7f23b0f6d982
“fbuba987” <115090534+fbuba987@users.noreply.github.com>
2022-11-08 16:37:08 -0300

    atualizacao

--- src/projetoIntegrador/Menu.java
+++ src/projetoIntegrador/Menu.java
@@ -3,16 +3,5 @@ package projetoIntegrador;
 
 public class Menu {
 
-import projetoIntegrador.controller.UsuarioController;
-import projetoIntegrador.model.Usuario;
-
-public class TestaPrograma {
-    static UsuarioController usuario = new UsuarioController();
-
-    public static void main(String[] args) {
-
-
-    }
-
 
 }

f02e99f62d7a7a010352edfb1c34c923207c643f
“fbuba987” <115090534+fbuba987@users.noreply.github.com>
2022-11-09 06:14:56 -0300

    Corrigindo a palavra carro

--- src/projetoIntegrador/model/Carro.java
+++ src/projetoIntegrador/model/Carro.java
@@ -1,12 +1,12 @@
 package projetoIntegrador.model;
 
 public class
-Corro extends Veiculo{
+Carro extends Veiculo{
 
     private String tração;
     private float motor;
 
-    public Corro(int id, String placa, String cor, String modelo, int ano, String combostivel) {
+    public Carro(int id, String placa, String cor, String modelo, int ano, String combostivel) {
         super(id, placa, cor, modelo, ano, combostivel);
     }
 }
